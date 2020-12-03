1c95e11dc0ff4fc285435fe9b40ba0ae4d75d9d6
Edson Martins <edson.martins@garupa.co>
2020-12-03 13:23:12 -0300

    aplicado lint e prettier

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import App from './presentation/App';
 
 const iniciar = async (): Promise<void> => {
   const app = new App();
-  const port = Number(process.env.PORT)
+  const port = Number(process.env.PORT);
   await app.initApp();
-  app.startServer(Number.isNaN(port) ? 3000 : port)
+  app.startServer(Number.isNaN(port) ? 3000 : port);
 };
 
 iniciar().catch((err) => console.log(err));
